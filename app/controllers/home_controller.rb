f2577ae0e812d35dc905f76e728519ed8b0722c9
alexfrancow <alejandrofrancovazquez@gmail.com>
2018-07-07 19:46:35 +0200

    Get JSON info from user

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,19 @@
 class HomeController < ApplicationController
   def index
   	@user = params[:search_user]
+
+  	if @user
+		require 'net/http'
+		require 'json'
+		url = 'https://www.instagram.com/'+@user+'/?hl=en'
+		uri = URI(url)
+		response = Net::HTTP.get(uri)
+		doc = Nokogiri::HTML(response)
+		@scripts = doc.css('script')
+		@scripts = @scripts.to_s
+		startstring = "_sharedData"
+		endstring = "</script"
+		@x = @scripts[/#{startstring}(.*?)#{endstring}/m, 1]
+	end
   end
 end
